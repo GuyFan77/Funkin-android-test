9ff34acaf93fb87a52c06924fedeb6164676dd44
GuyFan77 <oaleatorio371@gmail.com>
2024-05-07 20:43:29 -0300

    Update FreeplayState.hx

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -393,12 +393,14 @@ class FreeplayState extends MusicBeatState
 		
 
         
-		{
+		{	
+		
 	public var songName:String = "";
 	public var week:Int = 0;
 	public var songCharacter:String = "";
 
 	public function new(song:String, week:Int, songCharacter:String)
+	}
 	{
 		this.songName = song;
 		this.week = week;
